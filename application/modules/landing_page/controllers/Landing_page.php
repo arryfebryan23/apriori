7777a8a2651dbd7e2ce8f8a979ed4a6318f108dc
arryfebryan23 <arryfebryan23@gmail.com>
2022-06-18 21:43:40 +0700

    benerni landing page harus log in

--- application/modules/landing_page/controllers/Landing_page.php
+++ application/modules/landing_page/controllers/Landing_page.php
@@ -6,9 +6,6 @@ class Landing_page extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		if (!$this->ion_auth->logged_in()) {
-			redirect('auth/login');
-		}
 	}
 
 	public function index()
